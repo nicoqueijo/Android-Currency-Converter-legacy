ff000af4b5f0d148a8c5a998324808df873a2651
Nicolas Queijo <queijonicolas@gmail.com>
2018-09-08 02:49:51 -0400

    Refactored and documented code.

--- app/src/main/java/com/nicoqueijo/android/currencyconverter/fragments/SourceCodeFragment.java
+++ app/src/main/java/com/nicoqueijo/android/currencyconverter/fragments/SourceCodeFragment.java
@@ -11,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -20,9 +21,11 @@ import android.widget.ProgressBar;
 import com.nicoqueijo.android.currencyconverter.R;
 
 /**
- * Dialog Fragment used to view the source code of the project.
+ * Fragment used to view the source code of the project via an embedded browser window inside the
+ * app. Implements OnScrollChangedListener to monitor the scroll position.
  */
-public class SourceCodeFragment extends Fragment {
+public class SourceCodeFragment extends Fragment implements
+        ViewTreeObserver.OnScrollChangedListener {
 
     public static final String TAG = SourceCodeFragment.class.getSimpleName();
 
@@ -35,6 +38,27 @@ public class SourceCodeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_source_code, container, false);
+        initViews(view);
+        setUpWebClientsAndSwipeLayout();
+        handleBackPress();
+        return view;
+    }
+
+    /**
+     * Factory method to create a new instance of this fragment using the provided parameters.
+     *
+     * @return a new instance of fragment
+     */
+    public static SourceCodeFragment newInstance() {
+        return new SourceCodeFragment();
+    }
+
+    /**
+     * Initializes the views and configures the settings.
+     *
+     * @param view the root view of the inflated hierarchy
+     */
+    private void initViews(View view) {
         mWebView = view.findViewById(R.id.web_view);
         mProgressBar = view.findViewById(R.id.progress_bar);
         mSwipeRefreshLayout = view.findViewById(R.id.swipe_refresh_layout);
@@ -43,9 +67,14 @@ public class SourceCodeFragment extends Fragment {
         webViewSettings.setBuiltInZoomControls(true);
         webViewSettings.setDisplayZoomControls(false);
         mWebView.loadUrl("https://github.com/nicoqueijo");
+        mWebView.getViewTreeObserver().addOnScrollChangedListener(this);
+    }
 
+    /**
+     * Sets up the web clients by implementing how the ProgressBar interacts with them.
+     */
+    private void setUpWebClientsAndSwipeLayout() {
         mWebView.setWebViewClient(new WebViewClient() {
-
             @Override
             public void onPageStarted(WebView view, String url, Bitmap favicon) {
                 super.onPageStarted(view, url, favicon);
@@ -59,7 +88,6 @@ public class SourceCodeFragment extends Fragment {
                 mSwipeRefreshLayout.setRefreshing(false);
             }
         });
-
         mWebView.setWebChromeClient(new WebChromeClient() {
             @Override
             public void onProgressChanged(WebView view, int newProgress) {
@@ -67,15 +95,20 @@ public class SourceCodeFragment extends Fragment {
                 mProgressBar.setProgress(newProgress);
             }
         });
-
         mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
                 mWebView.reload();
             }
         });
+    }
 
-        // https://stackoverflow.com/a/47710213/5906793
+    /**
+     * Listens to the back button press and navigates backwards through the WebView if possible
+     * when pressed.
+     * Credit: https://stackoverflow.com/a/47710213/5906793
+     */
+    private void handleBackPress() {
         mWebView.setOnKeyListener(new View.OnKeyListener() {
             public boolean onKey(View v, int keyCode, KeyEvent event) {
                 if (keyCode == KeyEvent.KEYCODE_BACK
@@ -87,17 +120,19 @@ public class SourceCodeFragment extends Fragment {
                 return false;
             }
         });
-
-        return view;
     }
 
     /**
-     * Factory method to create a new instance of this fragment using the provided parameters.
-     *
-     * @return a new instance of fragment
+     * Listens for the WebView scroll position and assures the SwipeRefreshLayout is only enabled
+     * when the scroll position is at the top. This is to prevent the refresh feature to be
+     * triggered when the user swipes down to scroll.
      */
-    public static SourceCodeFragment newInstance() {
-        return new SourceCodeFragment();
+    @Override
+    public void onScrollChanged() {
+        if (mWebView.getScrollY() == 0) {
+            mSwipeRefreshLayout.setEnabled(true);
+        } else {
+            mSwipeRefreshLayout.setEnabled(false);
+        }
     }
-
 }
