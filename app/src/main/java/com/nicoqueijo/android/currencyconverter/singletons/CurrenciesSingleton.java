c5f157554342f20772ac93b639c37eef67cda926
Nicolas Queijo <queijonicolas@gmail.com>
2018-09-26 00:46:21 -0400

    Switch the retrieval of exchange rates from SharedPrefs to SQLite database.

--- app/src/main/java/com/nicoqueijo/android/currencyconverter/singletons/CurrenciesSingleton.java
+++ app/src/main/java/com/nicoqueijo/android/currencyconverter/singletons/CurrenciesSingleton.java
@@ -1,18 +1,14 @@
 package com.nicoqueijo.android.currencyconverter.singletons;
 
 import android.content.Context;
-import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 
-import com.nicoqueijo.android.currencyconverter.activities.MainActivity;
-import com.nicoqueijo.android.currencyconverter.helpers.Utility;
+import com.nicoqueijo.android.currencyconverter.databases.DatabaseConstants.TableAllCurrencies;
+import com.nicoqueijo.android.currencyconverter.databases.DatabaseHelper;
 import com.nicoqueijo.android.currencyconverter.models.Currency;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Map;
-
-import static android.content.Context.MODE_PRIVATE;
 
 /**
  * Singleton class for the main data set of the app which is the list of currencies along with
@@ -49,19 +45,44 @@ public class CurrenciesSingleton {
      * @param context Information about application environment. Needed to get SharedPrefs.
      */
     private void initAndSortCurrencies(Context context) {
-        SharedPreferences mSharedPrefsRates = context.getSharedPreferences(MainActivity
-                .sharedPrefsRatesFilename, MODE_PRIVATE);
-        Map<String, ?> keys = mSharedPrefsRates.getAll();
-        for (Map.Entry<String, ?> entry : keys.entrySet()) {
-            String currencyCode = entry.getKey();
-            double exchangeRate = Utility.getDouble(mSharedPrefsRates, entry.getKey(), 0.0);
+        DatabaseHelper databaseHelper = new DatabaseHelper(context);
+        SQLiteDatabase database = databaseHelper.getReadableDatabase();
+        Cursor cursor = database.query(
+                TableAllCurrencies.TABLE_NAME,
+                new String[]{
+                        TableAllCurrencies.COLUMN_CURRENCY_CODE,
+                        TableAllCurrencies.COLUMN_CURRENCY_VALUE},
+                null,
+                null,
+                null,
+                null,
+                TableAllCurrencies.COLUMN_CURRENCY_CODE + " ASC");
+
+        int col_currency_code = cursor.getColumnIndex(TableAllCurrencies.COLUMN_CURRENCY_CODE);
+        int col_currency_value = cursor.getColumnIndex(TableAllCurrencies.COLUMN_CURRENCY_VALUE);
+        String currencyCode;
+        double exchangeRate;
+
+        while (cursor.moveToNext()) {
+            currencyCode = cursor.getString(col_currency_code);
+            exchangeRate = cursor.getDouble(col_currency_value);
             mCurrencies.add(new Currency(currencyCode, exchangeRate));
         }
-        Collections.sort(mCurrencies, new Comparator<Currency>() {
-            @Override
-            public int compare(Currency currency1, Currency currency2) {
-                return currency1.getCurrencyCode().compareTo(currency2.getCurrencyCode());
-            }
-        });
+        cursor.close();
+
+//        SharedPreferences mSharedPrefsRates = context.getSharedPreferences(MainActivity
+//                .sharedPrefsRatesFilename, MODE_PRIVATE);
+//        Map<String, ?> keys = mSharedPrefsRates.getAll();
+//        for (Map.Entry<String, ?> entry : keys.entrySet()) {
+//            String currencyCode = entry.getKey();
+//            double exchangeRate = Utility.getDouble(mSharedPrefsRates, entry.getKey(), 0.0);
+//            mCurrencies.add(new Currency(currencyCode, exchangeRate));
+//        }
+//        Collections.sort(mCurrencies, new Comparator<Currency>() {
+//            @Override
+//            public int compare(Currency currency1, Currency currency2) {
+//                return currency1.getCurrencyCode().compareTo(currency2.getCurrencyCode());
+//            }
+//        });
     }
 }
