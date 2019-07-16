d1e8622d0f8ab7b1aeff7cdf83a6c02a9df59db1
Nicolas Queijo <queijonicolas@gmail.com>
2019-07-16 01:11:42 -0400

    Refactored code

--- app/src/main/java/com/nicoqueijo/android/currencyconverter/helpers/Utility.java
+++ app/src/main/java/com/nicoqueijo/android/currencyconverter/helpers/Utility.java
@@ -3,17 +3,18 @@ package com.nicoqueijo.android.currencyconverter.helpers;
 import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
-import androidx.databinding.BindingAdapter;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import com.google.android.material.snackbar.Snackbar;
-import androidx.fragment.app.DialogFragment;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.databinding.BindingAdapter;
+import androidx.fragment.app.DialogFragment;
+
+import com.google.android.material.snackbar.Snackbar;
 import com.nicoqueijo.android.currencyconverter.R;
 
 import java.lang.reflect.Method;
@@ -152,7 +153,6 @@ public final class Utility {
         int themeId = getThemeId(context);
         View snackbarView = snackbar.getView();
         int snackbarTextId = com.google.android.material.R.id.snackbar_text;
-//        int snackbarTextId = android.support.design.R.id.snackbar_text;
         TextView textView = snackbarView.findViewById(snackbarTextId);
         switch (themeId) {
             case R.style.AppThemeDark:
