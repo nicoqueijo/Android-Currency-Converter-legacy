64e03568e9af7ba2f4d78bdb4450055c396180a4
Nicolas Queijo <queijonicolas@gmail.com>
2019-07-25 00:05:25 -0400

    Created Room database singleton

--- app/src/main/java/com/nicoqueijo/android/currencyconverter/room/AppDatabase.java
+++
@@ -1,8 +0,0 @@
-package com.nicoqueijo.android.currencyconverter.room;
-
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-
-@Database(entities = {AllCurrency.class, ActiveCurrency.class}, version = 1)
-public abstract class AppDatabase extends RoomDatabase {
-}
---
+++ app/src/main/java/com/nicoqueijo/android/currencyconverter/room/CurrencyDatabase.java
@@ -0,0 +1,22 @@
+package com.nicoqueijo.android.currencyconverter.room;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {AllCurrency.class, ActiveCurrency.class}, version = 1)
+public abstract class CurrencyDatabase extends RoomDatabase {
+
+    private static final String DATABASE_NAME = "currency.db";
+    private static CurrencyDatabase instance;
+
+    public static synchronized CurrencyDatabase getInstance(Context context) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(context.getApplicationContext(),
+                    CurrencyDatabase.class, DATABASE_NAME).fallbackToDestructiveMigration().build();
+        }
+        return instance;
+    }
+}
