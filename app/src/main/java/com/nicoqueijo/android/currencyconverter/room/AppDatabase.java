32bdfa07a7c92e345b3548c62d3564c33b847797
Nicolas Queijo <queijonicolas@gmail.com>
2019-07-24 23:28:14 -0400

    Refactored Entities and Daos

--- app/src/main/java/com/nicoqueijo/android/currencyconverter/room/AppDatabase.java
+++ app/src/main/java/com/nicoqueijo/android/currencyconverter/room/AppDatabase.java
@@ -3,6 +3,6 @@ package com.nicoqueijo.android.currencyconverter.room;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {AllCurrencies.class, ActiveCurrencies.class}, version = 1)
+@Database(entities = {AllCurrency.class, ActiveCurrency.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
 }
