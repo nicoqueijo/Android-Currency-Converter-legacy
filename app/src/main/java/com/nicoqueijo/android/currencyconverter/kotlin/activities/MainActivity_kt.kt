eda38c1dc554273a391cca31400c7ac7e0634f4e
Nicolas Queijo <queijonicolas@gmail.com>
2020-02-12 12:29:58 -0500

    Add Constraint Layouts to activity_main.xml

--- app/src/main/java/com/nicoqueijo/android/currencyconverter/kotlin/activities/MainActivity_kt.kt
+++ app/src/main/java/com/nicoqueijo/android/currencyconverter/kotlin/activities/MainActivity_kt.kt
@@ -8,6 +8,6 @@ class MainActivity_kt : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_kt)
+        setContentView(R.layout.activity_main)
     }
 }
