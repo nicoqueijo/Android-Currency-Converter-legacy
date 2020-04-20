a92ad74ed23291b83532b9dfe3a6cf0ef527852b
Nicolas Queijo <queijonicolas@gmail.com>
2020-04-20 00:51:25 -0400

    Moved a lot of business logic from the Fragment to the ViewModel.

--- app/src/main/java/com/nicoqueijo/android/currencyconverter/kotlin/view/ActiveCurrenciesFragment.kt
+++ app/src/main/java/com/nicoqueijo/android/currencyconverter/kotlin/view/ActiveCurrenciesFragment.kt
@@ -24,12 +24,7 @@ import com.nicoqueijo.android.currencyconverter.R
 import com.nicoqueijo.android.currencyconverter.kotlin.model.Currency
 import com.nicoqueijo.android.currencyconverter.kotlin.util.Utils.Order.INVALID
 import com.nicoqueijo.android.currencyconverter.kotlin.util.Utils.copyToClipboard
-import com.nicoqueijo.android.currencyconverter.kotlin.util.Utils.elementAfter
-import com.nicoqueijo.android.currencyconverter.kotlin.util.Utils.elementBefore
-import com.nicoqueijo.android.currencyconverter.kotlin.util.Utils.hasOnlyOneElement
 import com.nicoqueijo.android.currencyconverter.kotlin.util.Utils.hide
-import com.nicoqueijo.android.currencyconverter.kotlin.util.Utils.isNotLastElement
-import com.nicoqueijo.android.currencyconverter.kotlin.util.Utils.isValid
 import com.nicoqueijo.android.currencyconverter.kotlin.util.Utils.show
 import com.nicoqueijo.android.currencyconverter.kotlin.util.Utils.vibrate
 import com.nicoqueijo.android.currencyconverter.kotlin.viewmodel.ActiveCurrenciesViewModel
@@ -60,7 +55,7 @@ class ActiveCurrenciesFragment : Fragment() {
         dragLinearLayout = view.findViewById(R.id.drag_linear_layout)
         dragLinearLayout.setContainerScrollView(scrollView)
         dragLinearLayout.setOnViewSwapListener { _, startPosition, _, endPosition ->
-            swapCurrencies(startPosition, endPosition)
+            viewModel.swapCurrencies(startPosition, endPosition)
         }
         keyboard = view.findViewById(R.id.keyboard)
         initFloatingActionButton(view)
@@ -74,15 +69,13 @@ class ActiveCurrenciesFragment : Fragment() {
             if (!viewModel.wasListConstructed) {
                 constructActiveCurrencies(databaseActiveCurrencies)
             }
-            if (wasCurrencyAddedViaFab(databaseActiveCurrencies)) {
+            if (viewModel.wasCurrencyAddedViaFab(databaseActiveCurrencies)) {
                 val addedCurrency = databaseActiveCurrencies.takeLast(1).single()
                 viewModel.memoryActiveCurrencies.add(addedCurrency)
                 addRow(addedCurrency)
             }
-            setDefaultFocus()
-            dragLinearLayout.forEachIndexed { i, it ->
-                styleIfFocused(viewModel.memoryActiveCurrencies[i], it as RowActiveCurrency)
-            }
+            viewModel.setDefaultFocus()
+            styleRows()
             toggleEmptyListViewVisibility()
         })
         /**
@@ -112,36 +105,11 @@ class ActiveCurrenciesFragment : Fragment() {
         }*/
     }
 
-    /**
-     * On the drag events, adjacent currencies need to swap position indices and this needs to be
-     * reflected in memory and in the database.
-     */
-    private fun swapCurrencies(firstPosition: Int, secondPosition: Int) {
-        viewModel.memoryActiveCurrencies[firstPosition].order = viewModel.memoryActiveCurrencies[secondPosition].order.also {
-            viewModel.memoryActiveCurrencies[secondPosition].order = viewModel.memoryActiveCurrencies[firstPosition].order
-        }
-        viewModel.memoryActiveCurrencies[firstPosition] = viewModel.memoryActiveCurrencies[secondPosition].also {
-            viewModel.memoryActiveCurrencies[secondPosition] = viewModel.memoryActiveCurrencies[firstPosition]
-        }
-        viewModel.upsertCurrency(viewModel.memoryActiveCurrencies[firstPosition])
-        viewModel.upsertCurrency(viewModel.memoryActiveCurrencies[secondPosition])
-    }
-
     private fun restoreActiveCurrencies() {
         viewModel.memoryActiveCurrencies.forEach { currency ->
             addRow(currency)
         }
     }
-
-    /**
-     * This indicates the user added a currency by selecting it from the SelectableCurrenciesFragment
-     * that was initiated by the press of the FloatingActionButton.
-     * The indication is triggered when, the only difference between [databaseActiveCurrencies] and
-     * [memoryActiveCurrencies] is that [databaseActiveCurrencies] has an extra element.
-     */
-    private fun wasCurrencyAddedViaFab(databaseActiveCurrencies: List<Currency>) =
-            (databaseActiveCurrencies.size - viewModel.memoryActiveCurrencies.size == 1 &&
-                    databaseActiveCurrencies.dropLast(1) == viewModel.memoryActiveCurrencies)
 
     /**
      * This inflates the DragLinearLayout with the active currencies from the database when the
@@ -150,13 +118,22 @@ class ActiveCurrenciesFragment : Fragment() {
     private fun constructActiveCurrencies(databaseActiveCurrencies: MutableList<Currency>?) {
         databaseActiveCurrencies?.forEach { currency ->
             viewModel.memoryActiveCurrencies.add(currency)
-            viewModel.setFocusToFirstCurrency()
             addRow(currency)
         }
         viewModel.wasListConstructed = true
     }
 
-    private fun styleIfFocused(currency: Currency, row: RowActiveCurrency) {
+    private fun styleRows() {
+        dragLinearLayout.forEachIndexed { i, row ->
+            styleRow(viewModel.memoryActiveCurrencies[i], row as RowActiveCurrency)
+        }
+    }
+
+    /**
+     * Styles the row in accordance to the focus state of its Currency. A row containing a focused
+     * Currency should have blinking cursor at the end of it's conversion field and a dark gray background.
+     */
+    private fun styleRow(currency: Currency, row: RowActiveCurrency) {
         row.run {
             if (currency.isFocused) {
                 rowCanvas.setBackgroundColor(ContextCompat.getColor(context, R.color.dark_gray))
@@ -169,7 +146,7 @@ class ActiveCurrenciesFragment : Fragment() {
     }
 
     /**
-     * Creates a row from a [currency] object, adds that row to the DragLinearLayout, and sets up
+     * Creates a row from a [currency], adds that row to the DragLinearLayout, and sets up
      * its listeners so it could be dragged, removed, and restored.
      */
     private fun addRow(currency: Currency) {
@@ -179,9 +156,6 @@ class ActiveCurrenciesFragment : Fragment() {
         dragLinearLayout.setViewDraggable(row, row)
 
         /**
-         * Current implementation: Longclicking the [currencyCode] area "removes" the currency from the list. The currency
-         *                         is hidden until the Snackbar is dismissed and at that point it is completely removed.
-         *
          * Implementation to add: Actually remove this currency from the memory currencies, database currencies,
          *                        dragLinearLayout children, if focused reassign the focus.
          */
@@ -189,7 +163,7 @@ class ActiveCurrenciesFragment : Fragment() {
 
             val currencyToRemove = viewModel.memoryActiveCurrencies[dragLinearLayout.indexOfChild(row)]
             val orderOfCurrencyToRemove = currencyToRemove.order
-            handleRemove(orderOfCurrencyToRemove)
+            viewModel.handleRemove(orderOfCurrencyToRemove)
             currencyToRemove.isSelected = false
             currencyToRemove.order = INVALID.position
             viewModel.upsertCurrency(currencyToRemove)
@@ -201,15 +175,11 @@ class ActiveCurrenciesFragment : Fragment() {
             dragLinearLayout.removeDragView(row)
             dragLinearLayout.layoutTransition = null
 
-            dragLinearLayout.forEachIndexed { i, view ->
-                styleIfFocused(viewModel.memoryActiveCurrencies[i], view as RowActiveCurrency)
-            }
+            styleRows()
 
             toggleEmptyListViewVisibility()
             Snackbar.make(dragLinearLayout, R.string.item_removed, Snackbar.LENGTH_SHORT)
                     /**
-                     * Current implementation: When the user clicks 'Undo' we restore the visibility of the currency.
-                     *
                      * Implementation to add: Re-add the removed currency at the position it was previously at.
                      *                        Restore the previous state of the database currencies, memory currencies,
                      *                        dragLinearLayout children, focusedCurrency.
@@ -239,103 +209,11 @@ class ActiveCurrenciesFragment : Fragment() {
          */
         row.conversion.setOnLongClickListener {
             activity?.copyToClipboard(row.conversion.text)
-            log("Memory currencies        : ${viewModel.memoryActiveCurrencies}")
-            log("Database currencies      : ${viewModel.databaseActiveCurrencies.value}")
-            log("DragLinearLayout children: ${dragLinearLayout.children.asSequence()
-                    .filter { it.visibility == View.VISIBLE }
-                    .map { (it as RowActiveCurrency).currencyCode.text.toString() }
-                    .toList()
-            }")
-            log("Focused currency         : ${viewModel.focusedCurrency.value}")
             true
         }
         row.conversion.setOnClickListener {
-            changeFocusedCurrency(dragLinearLayout.indexOfChild(row))
-            log("Memory currencies        : ${viewModel.memoryActiveCurrencies}")
-            log("Database currencies      : ${viewModel.databaseActiveCurrencies.value}")
-            log("DragLinearLayout children: ${dragLinearLayout.children.asSequence()
-                    .filter { it.visibility == View.VISIBLE }
-                    .map { (it as RowActiveCurrency).currencyCode.text.toString() }
-                    .toList()
-            }")
-            log("Focused currency         : ${viewModel.focusedCurrency.value}")
-        }
-    }
-
-    private fun handleRemove(positionOfLongClickedCurrency: Int) {
-        shiftCurrencies(positionOfLongClickedCurrency)
-        reassignFocusedCurrency(positionOfLongClickedCurrency)
-    }
-
-    /**
-     * Here we are reassigning the focus to another currency if the focused currency is removed.
-     * The reassignment logic is as follows:
-     *      If there are items below me:
-     *          Unfocus me, focus the item directly below me.
-     *      Else if I am the sole item:
-     *          Unfocus me;
-     *      Else:
-     *          Unfocus me, focus the item directly above me.
-     */
-    private fun reassignFocusedCurrency(positionOfLongClickedCurrency: Int) {
-        val removedCurrency = viewModel.memoryActiveCurrencies[positionOfLongClickedCurrency]
-        if (viewModel.focusedCurrency.value == removedCurrency) {
-            when {
-                viewModel.memoryActiveCurrencies.isNotLastElement(positionOfLongClickedCurrency) -> {
-                    viewModel.memoryActiveCurrencies.elementAfter(positionOfLongClickedCurrency).let { newlyFocusedCurrency ->
-                        newlyFocusedCurrency.isFocused = true
-                        removedCurrency.isFocused = false
-                        viewModel.focusedCurrency.value = newlyFocusedCurrency
-                    }
-                }
-                viewModel.memoryActiveCurrencies.hasOnlyOneElement() -> {
-                    viewModel.focusedCurrency.value = null
-                }
-                else -> {
-                    viewModel.memoryActiveCurrencies.elementBefore(positionOfLongClickedCurrency).let { newlyFocusedCurrency ->
-                        newlyFocusedCurrency.isFocused = true
-                        removedCurrency.isFocused = false
-                        viewModel.focusedCurrency.value = newlyFocusedCurrency
-                    }
-                }
-            }
-        }
-    }
-
-    private fun setDefaultFocus() {
-        if (viewModel.focusedCurrency.value == null && viewModel.memoryActiveCurrencies.isNotEmpty()) {
-            viewModel.focusedCurrency.value = viewModel.memoryActiveCurrencies.take(1).single()
-            viewModel.memoryActiveCurrencies.take(1).single().isFocused = true
-        }
-    }
-
-    private fun changeFocusedCurrency(positionOfClickedCurrency: Int) {
-        val clickedCurrency = viewModel.memoryActiveCurrencies[positionOfClickedCurrency]
-        val positionOfPreviouslyFocusedCurrency = viewModel.memoryActiveCurrencies.indexOf(viewModel.focusedCurrency.value)
-        if (positionOfPreviouslyFocusedCurrency.isValid()) {
-            viewModel.memoryActiveCurrencies[positionOfPreviouslyFocusedCurrency].isFocused = false
-        }
-        viewModel.focusedCurrency.value = clickedCurrency
-        clickedCurrency.isFocused = true
-        dragLinearLayout.forEachIndexed { i, it ->
-            styleIfFocused(viewModel.memoryActiveCurrencies[i], it as RowActiveCurrency)
-        }
-    }
-
-    /**
-     * All the currencies below the removed currency are shifted up one position to fill the gap.
-     */
-    private fun shiftCurrencies(orderOfCurrencyToRemove: Int) {
-        run loop@{
-            viewModel.memoryActiveCurrencies
-                    .reversed()
-                    .forEach { currency ->
-                        if (currency.order == orderOfCurrencyToRemove) {
-                            return@loop
-                        }
-                        currency.order--
-                        viewModel.upsertCurrency(currency)
-                    }
+            viewModel.changeFocusedCurrency(dragLinearLayout.indexOfChild(row))
+            styleRows()
         }
     }
 
@@ -364,13 +242,5 @@ class ActiveCurrenciesFragment : Fragment() {
         fun log(message: String) {
             Log.d("Nicoo", message)
         }
-
-        val currencyCodes = listOf(
-                "USD_ARS",
-                "USD_BRL",
-                "USD_CAD",
-                "USD_DKK",
-                "USD_EUR"
-        )
     }
 }
--- app/src/main/java/com/nicoqueijo/android/currencyconverter/kotlin/viewmodel/ActiveCurrenciesViewModel.kt
+++ app/src/main/java/com/nicoqueijo/android/currencyconverter/kotlin/viewmodel/ActiveCurrenciesViewModel.kt
@@ -10,7 +10,6 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.nicoqueijo.android.currencyconverter.kotlin.data.Repository
 import com.nicoqueijo.android.currencyconverter.kotlin.model.Currency
-import com.nicoqueijo.android.currencyconverter.kotlin.util.CurrencyConversion
 import com.nicoqueijo.android.currencyconverter.kotlin.util.Utils.Order.*
 import com.nicoqueijo.android.currencyconverter.kotlin.util.Utils.elementAfter
 import com.nicoqueijo.android.currencyconverter.kotlin.util.Utils.elementBefore
@@ -19,11 +18,9 @@ import com.nicoqueijo.android.currencyconverter.kotlin.util.Utils.isNotLastEleme
 import com.nicoqueijo.android.currencyconverter.kotlin.util.Utils.isValid
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import java.math.BigDecimal
 import java.text.DecimalFormat
 import java.text.NumberFormat
 import java.util.*
-import kotlin.properties.Delegates
 
 class ActiveCurrenciesViewModel(application: Application) : AndroidViewModel(application) {
 
@@ -45,12 +42,8 @@ class ActiveCurrenciesViewModel(application: Application) : AndroidViewModel(app
         repository.isFirstLaunch = value
     }
 
-    var adapterActiveCurrencies = mutableListOf<Currency>()
-    var memoryActiveCurrencies = mutableListOf<Currency>()
+    val memoryActiveCurrencies = mutableListOf<Currency>()
     val focusedCurrency = MutableLiveData<Currency?>()
-
-    private var swipedCurrency: Currency? = null
-    private var swipedCurrencyOrder by Delegates.notNull<Int>()
 
     private var decimalFormatter: DecimalFormat
     private var decimalSeparator: String
@@ -87,11 +80,6 @@ class ActiveCurrenciesViewModel(application: Application) : AndroidViewModel(app
             }
             else -> return false // never reached but kotlin requires it for exhaustiveness
         }
-    }
-
-    fun handleKeyLongPressed() {
-        triggerScrollToPosition()
-        focusedCurrency.value?.conversion?.conversionString = ""
     }
 
     private fun cleanInput(input: String): String {
@@ -153,46 +141,6 @@ class ActiveCurrenciesViewModel(application: Application) : AndroidViewModel(app
         return true
     }
 
-    fun setFocusToFirstCurrency() {
-        if (focusedCurrency.value == null && memoryActiveCurrencies.isNotEmpty()) {
-            focusedCurrency.value = memoryActiveCurrencies.take(1).single().also { firstCurrency ->
-                firstCurrency.isFocused = true
-            }
-        }
-    }
-
-    fun reconcileCurrencies(currencies: MutableList<Currency>) {
-        copyVolatileFields(currencies)
-        setFocusedCurrency(currencies)
-    }
-
-    private fun copyVolatileFields(freshCurrencies: MutableList<Currency>) {
-        val addedOrRestoredCurrency: Currency
-        if (freshCurrencies.size - adapterActiveCurrencies.size == 1 &&
-                focusedCurrency.value != null && focusedCurrency.value!!.conversion.conversionString.isNotEmpty()) {
-            addedOrRestoredCurrency = freshCurrencies.asSequence()
-                    .filter { !adapterActiveCurrencies.contains(it) }
-                    .first()
-            val fromRate = focusedCurrency.value?.exchangeRate
-            val toRate = addedOrRestoredCurrency.exchangeRate
-            val conversionValue = CurrencyConversion.convertCurrency(BigDecimal(
-                    focusedCurrency.value?.conversion?.conversionString?.replace(",", ".")), fromRate!!, toRate)
-            addedOrRestoredCurrency.conversion.conversionValue = conversionValue
-        }
-        adapterActiveCurrencies.forEach { adapterCurrency ->
-            if (adapterCurrency.order != INVALID.position && freshCurrencies.contains(adapterCurrency)) {
-                freshCurrencies[freshCurrencies.indexOf(adapterCurrency)] = adapterCurrency
-            }
-        }
-    }
-
-    private fun setFocusedCurrency(currencies: MutableList<Currency>) {
-        focusedCurrency.value?.let {
-            focusedCurrency.value = currencies[currencies.indexOf(it)]
-            currencies[currencies.indexOf(it)].isFocused = true
-        }
-    }
-
     /**
      *  The RecyclerView scrolls to the position of the focused currency when it is notified that
      *  the value of the focused currency has changed. This is a way to force that call.
@@ -201,116 +149,109 @@ class ActiveCurrenciesViewModel(application: Application) : AndroidViewModel(app
         focusedCurrency.value = focusedCurrency.value
     }
 
-    fun handleSwipe(position: Int): Int {
-        val indexToRefresh = reassignFocusedCurrency(position)
-        shiftCurrencies(position)
-        return indexToRefresh
+    /**
+     * Makes the currency at [positionOfClickedCurrency] focused and the previously focused
+     * currency unfocused. If the previously focused currency was removed it will not be in the
+     * list and the position will invalid.
+     */
+    fun changeFocusedCurrency(positionOfClickedCurrency: Int) {
+        val clickedCurrency = memoryActiveCurrencies[positionOfClickedCurrency]
+        val positionOfPreviouslyFocusedCurrency = memoryActiveCurrencies.indexOf(focusedCurrency.value)
+        if (positionOfPreviouslyFocusedCurrency.isValid()) {
+            memoryActiveCurrencies[positionOfPreviouslyFocusedCurrency].isFocused = false
+        }
+        focusedCurrency.value = clickedCurrency
+        clickedCurrency.isFocused = true
+    }
+
+    fun setDefaultFocus() {
+        if (focusedCurrency.value == null && memoryActiveCurrencies.isNotEmpty()) {
+            focusedCurrency.value = memoryActiveCurrencies.take(1).single().also { firstCurrency ->
+                firstCurrency.isFocused = true
+            }
+        }
+    }
+
+    fun handleRemove(positionOfLongClickedCurrency: Int) {
+        shiftCurrencies(positionOfLongClickedCurrency)
+        reassignFocusedCurrency(positionOfLongClickedCurrency)
     }
 
     /**
-     * Here we are reassigning the focus to another currency if the focused currency is swiped.
-     * The assignment logic is:
-     *      If there are items below me
-     *          Unfocus me, focus the item directly below me
-     *          Return position of item directly below me
-     *      Else if I am the sole item
-     *          Unfocus me
-     *          Return invalid position
-     *      Else
-     *          Unfocus me, focus the item directly above me
-     *          Return position of time directly above me
+     * Here we are reassigning the focus to another currency if the focused currency is removed.
+     * The reassignment logic is as follows:
+     *      If there are items below me:
+     *          Unfocus me, focus the item directly below me.
+     *      Else if I am the sole item:
+     *          Unfocus me;
+     *      Else:
+     *          Unfocus me, focus the item directly above me.
      */
-    private fun reassignFocusedCurrency(position: Int): Int {
-        swipedCurrency = adapterActiveCurrencies[position]
-        if (focusedCurrency.value == swipedCurrency) {
+    private fun reassignFocusedCurrency(positionOfLongClickedCurrency: Int) {
+        val removedCurrency = memoryActiveCurrencies[positionOfLongClickedCurrency]
+        if (focusedCurrency.value == removedCurrency) {
             when {
-                adapterActiveCurrencies.isNotLastElement(position) -> {
-                    adapterActiveCurrencies.elementAfter(position).let { newlyFocusedCurrency ->
+                memoryActiveCurrencies.isNotLastElement(positionOfLongClickedCurrency) -> {
+                    memoryActiveCurrencies.elementAfter(positionOfLongClickedCurrency).let { newlyFocusedCurrency ->
                         newlyFocusedCurrency.isFocused = true
-                        swipedCurrency?.isFocused = false
+                        removedCurrency.isFocused = false
                         focusedCurrency.value = newlyFocusedCurrency
-                        return adapterActiveCurrencies.indexOf(newlyFocusedCurrency)
                     }
                 }
-                adapterActiveCurrencies.hasOnlyOneElement() -> {
+                memoryActiveCurrencies.hasOnlyOneElement() -> {
                     focusedCurrency.value = null
-                    return INVALID.position
                 }
                 else -> {
-                    adapterActiveCurrencies.elementBefore(position).let { newlyFocusedCurrency ->
+                    memoryActiveCurrencies.elementBefore(positionOfLongClickedCurrency).let { newlyFocusedCurrency ->
                         newlyFocusedCurrency.isFocused = true
-                        swipedCurrency?.isFocused = false
+                        removedCurrency.isFocused = false
                         focusedCurrency.value = newlyFocusedCurrency
-                        return adapterActiveCurrencies.indexOf(newlyFocusedCurrency)
                     }
                 }
             }
         }
-        return INVALID.position
     }
 
-    private fun shiftCurrencies(position: Int) {
-        swipedCurrency = adapterActiveCurrencies[position]
-        swipedCurrencyOrder = swipedCurrency?.order!!
+    /**
+     * All the currencies below the removed currency are shifted up one position to fill in the gap.
+     */
+    private fun shiftCurrencies(orderOfCurrencyToRemove: Int) {
         run loop@{
-            adapterActiveCurrencies
+            memoryActiveCurrencies
                     .reversed()
                     .forEach { currency ->
-                        if (currency.order == swipedCurrencyOrder) {
+                        if (currency.order == orderOfCurrencyToRemove) {
                             return@loop
                         }
                         currency.order--
                         upsertCurrency(currency)
                     }
         }
-        swipedCurrency?.isSelected = false
-        swipedCurrency?.order = -1
-        upsertCurrency(swipedCurrency)
     }
 
     /**
-     * Makes the [newlyFocusedCurrency] focused and the previously focused currency unfocused.
-     * If the previously focused currency was swiped it will not be in the list and the index will
-     * return -1.
+     * This indicates the user added a currency by selecting it from the SelectableCurrenciesFragment
+     * that was initiated by the press of the FloatingActionButton.
+     * The indication is triggered when, the only difference between [databaseActiveCurrencies] and
+     * [memoryActiveCurrencies] is that [databaseActiveCurrencies] has an extra element.
      */
-    fun changeFocusedCurrency(newlyFocusedCurrency: Currency): Int {
-        val indexOfPreviouslyFocusedCurrency = adapterActiveCurrencies.indexOf(focusedCurrency.value)
-        if (indexOfPreviouslyFocusedCurrency.isValid()) {
-            adapterActiveCurrencies[indexOfPreviouslyFocusedCurrency].isFocused = false
-        }
-        focusedCurrency.value = newlyFocusedCurrency
-        newlyFocusedCurrency.isFocused = true
-        return indexOfPreviouslyFocusedCurrency
-    }
+    fun wasCurrencyAddedViaFab(databaseActiveCurrencies: List<Currency>) =
+            (databaseActiveCurrencies.size - memoryActiveCurrencies.size == 1 &&
+                    databaseActiveCurrencies.dropLast(1) == memoryActiveCurrencies)
 
-    fun handleSwipeUndo() {
-        swipedCurrency?.isSelected = true
-        swipedCurrency?.order = swipedCurrencyOrder
-        upsertCurrency(swipedCurrency)
-        for (i in swipedCurrencyOrder until adapterActiveCurrencies.size) {
-            val currency = adapterActiveCurrencies[i]
-            currency.order++
-            upsertCurrency(currency)
+    /**
+     * On the drag events, adjacent currencies need to swap position indices and this needs to be
+     * reflected in memory and in the database.
+     */
+    fun swapCurrencies(firstPosition: Int, secondPosition: Int) {
+        memoryActiveCurrencies[firstPosition].order = memoryActiveCurrencies[secondPosition].order.also {
+            memoryActiveCurrencies[secondPosition].order = memoryActiveCurrencies[firstPosition].order
         }
-    }
-
-    fun handleMove(oldPosition: Int, newPosition: Int) {
-        swapCurrencies(oldPosition, newPosition)
-    }
-
-    fun handleDrop() {
-        adapterActiveCurrencies.forEach {
-            upsertCurrency(it)
+        memoryActiveCurrencies[firstPosition] = memoryActiveCurrencies[secondPosition].also {
+            memoryActiveCurrencies[secondPosition] = memoryActiveCurrencies[firstPosition]
         }
-    }
-
-    private fun swapCurrencies(oldPosition: Int, newPosition: Int) {
-        adapterActiveCurrencies[oldPosition].order = adapterActiveCurrencies[newPosition].order.also {
-            adapterActiveCurrencies[newPosition].order = adapterActiveCurrencies[oldPosition].order
-        }
-        adapterActiveCurrencies[oldPosition] = adapterActiveCurrencies[newPosition].also {
-            adapterActiveCurrencies[newPosition] = adapterActiveCurrencies[oldPosition]
-        }
+        upsertCurrency(memoryActiveCurrencies[firstPosition])
+        upsertCurrency(memoryActiveCurrencies[secondPosition])
     }
 
     fun populateDefaultCurrencies() {
