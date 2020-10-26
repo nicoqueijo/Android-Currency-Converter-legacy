dab2e19003769afaf89bf322ab4753578010c074
Nicolas Queijo <queijonicolas@gmail.com>
2020-10-25 23:06:20 -0400

    Add Android tests for getting resources by name.

--- app/src/androidTest/java/com/nicoqueijo/android/currencyconverter/kotlin/util/UtilsAndroidTest.kt
+++ app/src/androidTest/java/com/nicoqueijo/android/currencyconverter/kotlin/util/UtilsAndroidTest.kt
@@ -1,13 +1,77 @@
 package com.nicoqueijo.android.currencyconverter.kotlin.util
 
-import org.junit.jupiter.api.Assertions
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import com.nicoqueijo.android.currencyconverter.R
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 internal class UtilsAndroidTest {
 
-    @Test
-    fun testSomething() {
-        Assertions.assertTrue(true)
+    private val context = ApplicationProvider.getApplicationContext<Context>()
+
+    @Nested
+    inner class GetStringByResourceName {
+        @Test
+        fun unitedStatesDollarReturnsCorrectString() {
+            val expected = "United States Dollar"
+            val actual = Utils.getStringResourceByName("USD_USD", context)
+            assertEquals(expected, actual)
+        }
+
+        @Test
+        fun britishPoundReturnsCorrectString() {
+            val expected = "British Pound"
+            val actual = Utils.getStringResourceByName("USD_GBP", context)
+            assertEquals(expected, actual)
+        }
+
+        @Test
+        fun euroReturnsCorrectString() {
+            val expected = "Euro"
+            val actual = Utils.getStringResourceByName("USD_EUR", context)
+            assertEquals(expected, actual)
+        }
+
+        @Test
+        fun bitcoinReturnsCorrectString() {
+            val expected = "Bitcoin"
+            val actual = Utils.getStringResourceByName("USD_BTC", context)
+            assertEquals(expected, actual)
+        }
     }
 
+    @Nested
+    inner class GetDrawableResourceByName {
+        @Test
+        fun unitedStatesDollarReturnsCorrectDrawable() {
+            val expected = R.drawable.usd_usd
+            val actual = Utils.getDrawableResourceByName("usd_usd", context)
+            assertEquals(expected, actual)
+        }
+
+        @Test
+        fun britishPoundReturnsCorrectDrawable() {
+            val expected = R.drawable.usd_gbp
+            val actual = Utils.getDrawableResourceByName("usd_gbp", context)
+            assertEquals(expected, actual)
+        }
+
+        @Test
+        fun euroReturnsCorrectDrawable() {
+            val expected = R.drawable.usd_eur
+            val actual = Utils.getDrawableResourceByName("usd_eur", context)
+            assertEquals(expected, actual)
+        }
+
+        @Test
+        fun bitcoinReturnsCorrectDrawable() {
+            val expected = R.drawable.usd_btc
+            val actual = Utils.getDrawableResourceByName("usd_btc", context)
+            assertEquals(expected, actual)
+        }
+    }
 }
--- app/src/test/java/com/nicoqueijo/android/currencyconverter/kotlin/util/CurrencyConversionTest.kt
+++ app/src/test/java/com/nicoqueijo/android/currencyconverter/kotlin/util/CurrencyConversionTest.kt
@@ -9,6 +9,35 @@ import java.math.BigDecimal
 internal class CurrencyConversionTest {
 
     /**
+     * Pool of a few selected currencies for testing.
+     * Currency to Exchange Rate.
+     */
+    private val exchangeRates = mapOf(
+            "AED" to 3.673, // Emirati Dirham
+            "ARS" to 78.120127, // Argentine Peso
+            "AUD" to 1.400757, // Australian Dollar
+            "BRL" to 5.619251, // Brazilian Real
+            "BTC" to 0.000076918096, // Bitcoin
+            "CAD" to 1.312575, // Canadian Dollar
+            "CHF" to 0.904143, // Swiss Franc
+            "CNY" to 6.6868, // Chinese Yuan Renminbi
+            "COP" to 3778.616587, // Colombian Peso
+            "EUR" to 0.842993, // Euro
+            "GBP" to 0.766548, // British Pound
+            "HKD" to 7.750215, // Hong Kong Dollar
+            "INR" to 73.83415, // Indian Rupee
+            "JPY" to 104.70502716, // Japanese Yen
+            "KRW" to 1128.445, // South Korean Won
+            "MXN" to 20.86345, // Mexican Peso
+            "RUB" to 76.2173, // Russian Ruble
+            "SGD" to 1.3581, // Singapore Dollar
+            "USD" to 1.0, // United States Dollar
+            "VEF" to 248487.642241, // Venezuelan Bolívar
+            "XAG" to 0.04065871, // Silver
+            "XAU" to 0.00052584, // Gold
+    )
+
+    /**
      * Most traded currency pairings. Amount value of 100.
      */
     @Test
@@ -246,33 +275,4 @@ internal class CurrencyConversionTest {
         val actual = CurrencyConversion.convertCurrency(amount, from!!, to!!)
         assertEquals(expected.toDouble(), actual.toDouble())
     }
-
-    /**
-     * Pool of a few selected currencies for testing.
-     * Currency to Exchange Rate
-     */
-    private val exchangeRates = mapOf(
-            "AED" to 3.673, // Emirati Dirham
-            "ARS" to 78.120127, // Argentine Peso
-            "AUD" to 1.400757, // Australian Dollar
-            "BRL" to 5.619251, // Brazilian Real
-            "BTC" to 0.000076918096, // Bitcoin
-            "CAD" to 1.312575, // Canadian Dollar
-            "CHF" to 0.904143, // Swiss Franc
-            "CNY" to 6.6868, // Chinese Yuan Renminbi
-            "COP" to 3778.616587, // Colombian Peso
-            "EUR" to 0.842993, // Euro
-            "GBP" to 0.766548, // British Pound
-            "HKD" to 7.750215, // Hong Kong Dollar
-            "INR" to 73.83415, // Indian Rupee
-            "JPY" to 104.70502716, // Japanese Yen
-            "KRW" to 1128.445, // South Korean Won
-            "MXN" to 20.86345, // Mexican Peso
-            "RUB" to 76.2173, // Russian Ruble
-            "SGD" to 1.3581, // Singapore Dollar
-            "USD" to 1.0, // United States Dollar
-            "VEF" to 248487.642241, // Venezuelan Bolívar
-            "XAG" to 0.04065871, // Silver
-            "XAU" to 0.00052584, // Gold
-    )
 }
